521dcd8d7934c3e1f4aa8898ce93d712850ff29b
Jonghoon Yoon <by.newy@gmail.com>
2025-09-14 16:30:06 +0900

    feat: 설정 파일 추가

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = import.meta.env.VITE_APP_NAME;
+export const LICENSE_API_URL = import.meta.env.VITE_LICENSE_API_URL;
+export const LICENSE_API_KEY = import.meta.env.VITE_LICENSE_API_KEY;
